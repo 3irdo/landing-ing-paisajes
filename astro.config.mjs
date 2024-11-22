768f30d2a919902d4cb3f080c502e910c34e92e8
3irdo <sgvmoe@gmail.com>
2024-11-21 20:31:17 -0500

    contact form working, thanks page added, ready to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  base: "ingenieria_paisajes"
+  integrations: [tailwind(), react()]
+  // base: "ingenieria_paisajes"
   });
--- src/env.d.ts
+++ src/env.d.ts
@@ -2,5 +2,4 @@
 
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-FORMTOKEN = "232c90b0-18f2-4ccf-9174-023c573656af"
 
