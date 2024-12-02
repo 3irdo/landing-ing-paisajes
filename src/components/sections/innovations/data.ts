8a4dd467022462b73d58ebffea27edfa1a4b23a8
3irdo <sgvmoe@gmail.com>
2024-12-02 16:50:23 -0500

    claning to  build

--- src/components/sections/innovations/data.ts
+++ src/components/sections/innovations/data.ts
@@ -1,23 +1,20 @@
 import vivero from "./assets/vivero.webp";
 import paisajismo from "./assets/jardin.webp";
 import mantenimiento from "./assets/mantenimiento.webp";
-import hidrosiembra from './assets/hidrosiembra.webp'
-import viveroForestal from './assets/viveroArboles.webp'
-import sau from './assets/sau.webp'
-import bird from './assets/bird.jpg'
-import inventarioForestal from './assets/inventarioForestal.webp'
-import tala from './assets/tala.webp'
-import salvoconducto from './assets/tramite.jpg'
-import hand from './assets/hand.webp'
-import worms from './assets/worm.webp'
-import workers from './assets/wokers.webp'
-import forest from './assets/forest.webp'
-import civil from './assets/civilJob.webp'
-
-
+import hidrosiembra from "./assets/hidrosiembra.webp";
+import viveroForestal from "./assets/viveroArboles.webp";
+import sau from "./assets/sau.webp";
+import bird from "./assets/bird.jpg";
+import inventarioForestal from "./assets/inventarioForestal.webp";
+import tala from "./assets/tala.webp";
+import salvoconducto from "./assets/tramite.jpg";
+import hand from "./assets/hand.webp";
+import worms from "./assets/worm.webp";
+import workers from "./assets/wokers.webp";
+import forest from "./assets/forest.webp";
+import civil from "./assets/civilJob.webp";
 
 export const servicesData = {
- 
   subheading1_1: "Transforma tu entorno con nuestros servicios de ",
   subheading1_2: "paisajismo de vanguardia",
   innovations: [
@@ -26,21 +23,18 @@ export const servicesData = {
       description:
         "En Ingeniería y Paisajes SAS, contamos con vivero propio en el cual suministramos todo tipo de especies nativas y de jardinería para cualquier tipo de siembras.",
       visual: vivero,
-
     },
     {
       title: "DISEÑO PAISAJÍSTICO",
       description:
         "Con nuestros profesionales realizamos todo tipo de diseño paisajístico mejorando la apariencia física de espacios exteriores, ya sea en un área residencial, un parque urbano o un espacio comercial. Con el fin de crear un entorno armonioso y funcional",
       visual: paisajismo,
-      
     },
     {
       title: "MANTENIMIENTO DE ZONAS VERDES",
       description:
         "Contamos con todas las herramientas y maquinarias necesarias para realizar mantenimientos de zonas verdes (vías, empresas, residencial) y con el personal especializado.",
       visual: mantenimiento,
-     
     },
     {
       title: "HIDROSIEMBRAS",
@@ -55,7 +49,6 @@ export const servicesData = {
 };
 
 export const forestalesData = {
- 
   subheading1_1: "Sé responsable con el  medio ambiente con nuestros",
   subheading1_2: " Servicios Forestales",
   innovations: [
@@ -64,43 +57,36 @@ export const forestalesData = {
       description:
         "Suministramos de nuestro vivero todo tipo de plantas y arboles que se necesiten para la compensación ambiental luego de actividades de aprovechamiento forestal.",
       visual: viveroForestal,
-      
     },
     {
       title: "INGRESO SAU",
       description:
         "Ingresamos al sistema de árbol urbano (SAU) todas las intervenciones que se realizan sobre los árboles ubicado en zona urbana, de tal forma que cada uno de ellos cuente con su historial de mantenimiento.",
       visual: sau,
-      
     },
     {
       title: "PLANES DE MANEJO DE FAUNA Y FLORA",
       description:
         "Diseñamos planes que describen y programan las actividades para el manejo de especies de fauna y flora particulares y sus hábitats, además establecemos metas e indicadores de éxito en función del hábitat y las poblaciones.",
       visual: bird,
-      
     },
     {
       title: "INVENTARIOS FORESTALES",
       description:
         "Según La Organización de las Naciones Unidas para la Alimentación y la Agricultura (FAO), Un inventario forestal consiste en la recolección sistemática de datos sobre los recursos forestales de una zona determinada.\nEn ingeniería y paisajes contamos con el personal calificado para estas actividades, en las cuales realizamos la evaluación del estado actual y se sientan las bases del análisis y la planificación que constituyen el punto de partida de una gestión forestal sostenible.",
       visual: inventarioForestal,
-      location: "Medellín - Antioquia",
-      
     },
     {
       title: "APROVECHAMIENTO FORESTAL Y PODAS",
       description:
         "Contamos con el personal operativo especializado en estas actividades, somos especialistas en talas y podas controladas de individuos arbóreos teniendo en cuenta minuciosamente las recomendaciones estipuladas en cada resolución.",
       visual: tala,
-      
     },
     {
       title: "EMISIÓN DE SALVOCONDUCTOS",
       description:
         "Desde Ingeniería y Paisajes SAS, gestionamos en las diferentes autoridades ambientales los permisos únicos nacionales para la movilización de especímenes de la diversidad biológica.",
       visual: salvoconducto,
-      
     },
   ],
   visual: "innovations-section-image.jpg",
@@ -109,8 +95,8 @@ export const forestalesData = {
 };
 
 export const ambientalesData = {
- 
-  subheading1_1: "Transforma tu entorno con nuestros servicios de paisajismo de vanguardia.",
+  subheading1_1:
+    "Transforma tu entorno con nuestros servicios de paisajismo de vanguardia.",
   subheading1_2: " Servicios Ambientales",
   innovations: [
     {
@@ -118,35 +104,30 @@ export const ambientalesData = {
       description:
         "Con nuestros ingenieros ambientales, formulamos e implementamos planes de manejo ambiental para empresas y obras civiles, mitigando con esto los impactos ambientales causados por las actividades que realizan.",
       visual: hand,
-      
     },
     {
       title: "PLAN DE GESTIÓN DE RESIDUOS",
       description:
         "En ingeniería y paisajes S.A.S formulamos planes de gestión de residuos en los cuales establecemos objetivos, metas, programas, proyectos, actividades y recursos para el manejo integral de los residuos sólidos.",
       visual: worms,
-      
     },
     {
       title: "SERVICIOS AMBIENTALES ESTUDIOS DE IMPACTO AMBIENTAL",
       description:
         "Con nuestro equipo interdisciplinario de profesionales realizamos estudios de impacto ambiental a aquellos proyectos, obras o actividades que requieren licencia ambiental.",
       visual: workers,
-      
     },
     {
       title: "TRÁMITES AMBIENTALES",
       description:
         "Gestionamos todo tipo de permisos ante las autoridades ambientales a personas naturales o jurídicas y a las entidades gubernamentales.",
       visual: forest,
-      
     },
     {
       title: "PLANES DE GESTIÓN AMBIENTAL EN OBRAS CIVILES ",
       description:
         "En Ingeniería y Paisajes SAS tenemos una gran experiencia en planes de manejo ambiental aplicados a las obras civiles que generan impactos significativos al ambiente mediante sus procesos constructivos.",
       visual: civil,
-      
     },
   ],
   visual: "innovations-section-image.jpg",
