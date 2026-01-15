4ee8d1eeee91a2a12c6be313e0c8773634e5a4f8
Birdo <70602394+3irdo@users.noreply.github.com>
2026-01-14 21:01:01 -0500

    Update site URL in astro.config.mjs
    from .com to .org

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ingenieriaypaisajes.com',
+  site: 'https://ingenieriaypaisajes.org',
   integrations: [tailwind(), react(), sitemap()],
   });
